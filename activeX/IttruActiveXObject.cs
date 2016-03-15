2759ca9a1c775b11e999cb1d6b579c2988dfdd1b
sergioleal <sergio.leal@gmail.com>
2016-03-15 15:56:00 -0300

    acertos no instalador

--- activeX/IttruActiveXObject.cs
+++ activeX/IttruActiveXObject.cs
@@ -37,7 +37,7 @@ namespace ittru
     //
     [
         ComImport(),
-        Guid("CB5BDC81-93C1-11CF-8F20-00805F2CD064"),
+        Guid("CB5BDC81-93C1-11CF-8F20-11805F2CD064"),
         InterfaceType(ComInterfaceType.InterfaceIsIUnknown)
     ]
     public interface IObjectSafety
